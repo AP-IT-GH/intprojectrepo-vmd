fc374a3b9c01cff1f5a392f93769b39fd65564fe
JensVG <jens.vangoethem@student.ap.be>
2020-02-19 13:12:48 +0100

    Humidity V1.0

--- src/app/pages/humidity/humidity.page.ts
+++ src/app/pages/humidity/humidity.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from "chart.js";
 
 @Component({
   selector: 'app-humidity',
@@ -6,10 +7,110 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./humidity.page.scss'],
 })
 export class HumidityPage implements OnInit {
+  
+  @ViewChild("barCanvas", {static:true}) barCanvas: ElementRef;
+  @ViewChild("doughnutCanvas", {static:true}) doughnutCanvas: ElementRef;
+  @ViewChild("lineCanvas", {static:true}) lineCanvas: ElementRef;
+
+  private barChart: Chart;
+  private doughnutChart: Chart;
+  private lineChart: Chart;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: "bar",
+      data: {
+        labels: ["Monday", "Teusday", "Wednesday", "Thunderday", "Saterday", "Sunday"],
+        datasets: [
+          {
+            label: "# of humidity",
+            data: [83, 63, 41, 58, 82, 28],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
 
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: "doughnut",
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56"]
+          }
+        ]
+      }
+    });
+
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM", "05:00 PM", "06:00 PM", "07:00 PM"],
+        datasets: [
+          {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [23, 27, 25, 29, 28, 32, 35],
+            spanGaps: false
+          }
+        ]
+      }
+    });
+  }
 }
