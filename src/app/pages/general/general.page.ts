bd415b937f296ba6557c1058d919e88df47df926
jens van goethem <jens.vangoethem@student.ap.be>
2020-04-01 00:06:29 +0200

    Update

--- src/app/pages/general/general.page.ts
+++ src/app/pages/general/general.page.ts
@@ -1,6 +1,6 @@
 import { ThemeService } from './../../services/theme.service';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import {interval} from  'rxjs';
+import { interval } from 'rxjs';
 import { APIService, IAllDeviceData, IDevice } from 'src/app/Services/api.service';
 
 @Component({
@@ -10,39 +10,47 @@ import { APIService, IAllDeviceData, IDevice } from 'src/app/Services/api.servic
 })
 
 export class GeneralPage implements OnInit {
-
+  //index = 1;
+  //className: string = '';
   DataDevice: IAllDeviceData;
   Device: IDevice[];
-  constructor(private APIService: APIService) { 
+  constructor(private APIService: APIService) {
     interval(5000).subscribe(x => { // will execute every 30 seconds
       this.GetLatestData();
     });
     /*
-    var getal = this.Device[0].Status;
-    if( getal = 0){
-      document.getElementById('div')[0].className = "offline";
+    while (this.index > -1) {
+      var getal = this.Device[this.index].Status;
+      if (getal = 0) {
+        this.className = 'offline';
+      }
+      else if (getal = 1) {
+        this.className = 'online';
+      }
+      else if (getal = 2) {
+        this.className = 'sleepmodus';
+      }
+      this.index++;
     }
-    if( getal = 1){
-      document.getElementById('div')[0].className = "online";
-    }
-    console.log(getal);
     */
   }
 
- async ngOnInit() {
-  this.APIService.GetLatestDeviceInfo(1).subscribe(DataDevice =>{
-  this.DataDevice = DataDevice;
-})
-this.APIService.GetDeviceInfogeneral().subscribe(Device =>{
-  this.Device = Device;
-})}
-GetLatestData(){
-  this.APIService.GetLatestDeviceInfo(1).subscribe(DataDevice =>{
-    this.DataDevice = DataDevice;
-})}
-GetDeviceData(){
-  this.APIService.GetDeviceInfogeneral().subscribe(Device =>{
-    this.Device = Device;
-  })
-}
+  async ngOnInit() {
+    this.APIService.GetLatestDeviceInfo(1).subscribe(DataDevice => {
+      this.DataDevice = DataDevice;
+    })
+    this.APIService.GetDeviceInfogeneral().subscribe(Device => {
+      this.Device = Device;
+    })
+  }
+  GetLatestData() {
+    this.APIService.GetLatestDeviceInfo(1).subscribe(DataDevice => {
+      this.DataDevice = DataDevice;
+    })
+  }
+  GetDeviceData() {
+    this.APIService.GetDeviceInfogeneral().subscribe(Device => {
+      this.Device = Device;
+    })
+  }
 }
