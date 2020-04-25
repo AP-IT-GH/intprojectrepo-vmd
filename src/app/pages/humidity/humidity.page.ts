7c6128e5b28021b6cae77cb13a2ad7ed58fcc9c7
Arne Janssens <arne.janssens@student.ap.be>
2020-04-25 17:36:52 +0200

    Update Humid Pages

--- src/app/pages/humidity/humidity.page.ts
+++ src/app/pages/humidity/humidity.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Chart, ChartDataSets } from "chart.js";
 import { APIService, IAllDeviceData } from 'src/app/Services/api.service';
-import { ToastController } from '@ionic/angular';
+import { ToastController, Platform } from '@ionic/angular';
 import { interval } from 'rxjs';
 import { DatePipe } from '@angular/common';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { IexeedEntry, StorageService } from 'src/app/Services/storage.service';
+
 
 @Component({
   selector: 'app-humidity',
@@ -14,6 +15,9 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
 })
 export class HumidityPage implements OnInit {
 
+  humidEntries: IexeedEntry[] = [];
+  newHumidEntry: IexeedEntry = <IexeedEntry>{};
+  rangeCountEntry: IexeedEntry = <IexeedEntry>{};
   chartData: ChartDataSets[] = [{ data: [], label: 'Humidity', fill: false}];
   chartLabels: String[];
 
@@ -50,18 +54,55 @@ export class HumidityPage implements OnInit {
     chartType = 'line';
     showLegend = false;
     
-  constructor(public toastController:ToastController, private APIService: APIService,public datepipe: DatePipe) { 
-    this.GetAllInfoDevice();
-    interval(60000).subscribe(x => { // will execute every minute
-    this.GetAllInfoDevice();
-  });
-  }
+  constructor(public toastController:ToastController,
+    private plt: Platform,
+    private APIService: APIService,
+    private storage: StorageService,
+    public datepipe: DatePipe) { 
+      this.plt.ready().then(() => {
+        this.loadHumidEntries();
+      })
+
+      this.GetLatestData();
+      this.GetAllInfoDevice();
+      interval(60000).subscribe(x => { // will execute every minute
+        this.GetLatestData();
+        this.GetAllInfoDevice();
+      });
+    }
 
   async ngOnInit() {
-        //limiet instellen
-        this.APIService.GetLatestSingleDeviceInfo(1).subscribe(DataDevice=>{ //device ID moet een variabele zijn in de toekomst.
-          this.DataDevice = DataDevice;
-        })
+    //limiet instellen
+    this.APIService.GetLatestSingleDeviceInfo(1).subscribe(DataDevice => { //device ID moet een variabele zijn in de toekomst.
+      this.DataDevice = DataDevice;
+    })
+  }
+
+  //Add Entry
+  addHumidEntry(humidEntry: IexeedEntry) {
+    this.newHumidEntry.id = Date.now();
+    this.storage.addEntry(humidEntry).then(entry => {
+      this.newHumidEntry = <IexeedEntry>{};
+      this.showToast('Humidity Entry Added');
+      this.loadHumidEntries();
+    })
+  }
+
+  //Load Entries
+  loadHumidEntries(){
+    this.storage.getEntries().then(humidEntries => {
+      this.humidEntries = humidEntries;
+    })
+  }
+
+  //Remove Entry
+  removeHumidEntry(ID: number) {
+    this.storage.deleteEntry(ID);
+  }
+
+  //Remove All Entries
+  removeAllHumidEntries(){
+    this.storage.deleteAllEntries();
   }
 
   GetAllInfoDevice() {
@@ -80,11 +121,52 @@ export class HumidityPage implements OnInit {
     })
   }
   
+  typeChanged(e){
+    const on = e.detail.checked;
+    this.chartType = on ? 'line' : 'bar';
+  }
 
-  
-    typeChanged(e){
-      const on = e.detail.checked;
-      this.chartType = on ? 'line' : 'bar';
-    }
+  //instellen van een limiet: 
+  public rangeCount: number = 50;
+  public packetNumber: number = 100;
+  private lastSavedDate: Date;
+
+  GetLatestData() {
+    this.APIService.GetLatestSingleDeviceInfo(1).subscribe(DataDevice => {
+      this.DataDevice = DataDevice;
+      if (this.DataDevice.Temperature > this.rangeCount && this.DataDevice.Date != this.lastSavedDate
+      ) {
+
+        this.newHumidEntry.temperature = this.DataDevice.Temperature;
+        this.newHumidEntry.date = this.DataDevice.Date;
+        this.newHumidEntry.time = this.DataDevice.Time;
+        this.lastSavedDate = this.DataDevice.Date;
+        this.addHumidEntry(this.newHumidEntry)
+
+        //* Notification
+        var message = {
+          app_id: "b16686d2-04a8-468a-8658-7b411f0a777b",
+          contents: { "en": "The humidity is higher than your given humidity!" }, //placeholder text
+          included_segments: ["All"]
+        };
+
+        this.APIService.SendNotification(message).subscribe(data => {
+          console.log('The humidity is higher than your given humidity!');
+          console.log(data);
+        },
+        err => {
+          alert(err);
+        });
+      }
+    });
+  }
+
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 200
+    });
+    toast.present();
+  }
 }
   
