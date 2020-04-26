0c0ced0c51c26cee97386bd163878306089bbd49
Maxime Minta <maxime.minta@hotmail.com>
2020-04-26 22:01:58 +0200

    Bluetooth + APP maakt nu verbinding met API

--- src/app/pages/finddevice/finddevice.page.ts
+++ src/app/pages/finddevice/finddevice.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, NgZone } from '@angular/core';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { BLE } from '@ionic-native/ble/ngx';
 import { async } from '@angular/core/testing';
+import { SubjectSubscriber } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-finddevice',
@@ -15,56 +16,98 @@ export class FinddevicePage implements OnInit {
   BluetoothDevices: string[];
   SelectedBluetoothDevice: any;
 
-  public FoundDevices:any = [];
-  constructor(private bluetoothSerial: BluetoothSerial, private ble:BLE, private ngZone:NgZone) 
-  {
+  public FoundDevices: any = [];
+  public pairedDeviceID: number = 0;
+  private datasend: string ="";
+  constructor(private bluetoothSerial: BluetoothSerial, private ble: BLE, private ngZone: NgZone) {
+    this.checkBluetoothEnabled();
+
   }
 
   ngOnInit() {
-    this.bluetoothSerial.enable();
+
     //this.getAllBluetoothDevices();
   }
 
-  async SendCredentials(){
-    await this.ble.connect(this.SelectedBluetoothDevice).subscribe(data => alert("data" + JSON.stringify(data,null,2)));
-    if(!this.password || !this.ssid){
-      return alert("Incorrect input!");
-    } else{
-    await this.ble.write(this.SelectedBluetoothDevice,this.SelectedBluetoothDevice,this.SelectedBluetoothDevice,this.stringToBytes(this.password));
-    await this.ble.write(this.SelectedBluetoothDevice, this.SelectedBluetoothDevice, this.SelectedBluetoothDevice, this.stringToBytes(this.ssid))
-    }
+  checkBluetoothEnabled() {
+    this.bluetoothSerial.isEnabled().then(success => {
+      //this.FoundDevices();
+      this.listpairedDevices();
+    }, error => {
+      alert("Please Enable Bluetooth");
+    });
   }
 
-
-  stringToBytes(string:string){
-    
-    var array = new Uint8Array(string.length);
-    for (var i = 0, l = string.length; i< l; i++){
-      array[i] = string.charCodeAt(i);
+    //add paired devices to list
+    listpairedDevices(){
+      this.bluetoothSerial.list().then(success =>{
+        success.forEach(element => {
+          this.FoundDevices.push(element)
+        });
+      }, error => {
+        alert("Please enable bluetooth!");     
+      });
     }
-    return array.buffer
-    
-    
+
+    selectDevice(){
+      let connectedDevice = this.FoundDevices[this.pairedDeviceID];
+      if(!connectedDevice.address){
+        alert("Select a device to connect.");
+        return;
+      }
+      let address = connectedDevice.address;
+      let name = connectedDevice.name;
+  
+      this.connect(address)
+    }
+  
+    //create connection
+    connect(address){
+      this.bluetoothSerial.connect(address).subscribe(success =>{
+        this.deviceConnected();
+      }, error =>{
+        alert("Error: Connection to device." + error);//showtoast?
+      });
+    }
+  
+    deviceConnected(){
+      this.bluetoothSerial.subscribe('\n').subscribe(success => {
+        alert(success);
+        alert("Device Connected!");
+        //toast, connected successfully
+      }, error => {
+        alert(error);
+      });
+    }
+  
+    deviceDisconnected(){
+      this.bluetoothSerial.disconnect();
+      alert("Device Disconnected");
+    }
+  
+    sendData(data){
+      //this.datasend+='\n';
+      this.bluetoothSerial.write(data).then(succes =>{
+        alert(succes);
+        alert("Data Sent!");
+      }, error => {
+        alert(error);
+      });
+    }
+
+
+    //SEND DATA
+  async SendCredentials() {
+
+    //make a connection first
+    await this.connect(this.SelectedBluetoothDevice);
+
+     if (!this.password || !this.ssid) {
+       return alert("Incorrect input!");
+     } else {
+       await this.sendData(this.password);
+       await this.sendData(this.ssid);
+     }
   }
-
-
-  async getAllBluetoothDevices(){
-    this.ble.scan([],15).subscribe(device=> this.onDeviceDiscovered(device));
-   }
-
-   onDeviceDiscovered(device){
-     //alert('Discovered' + JSON.stringify(device,null,2));
-
-     this.ngZone.run(() =>{
-       this.FoundDevices.push(device);
-       //alert("Device Name: " + device.name + " DEVICE ID:" + device.id + " RSSI: " + device.rssi );
-     });
-   }
-
 }
 
-export interface IBluetoothDevices{
-  id: string,
-  rssi: string,
-  name: string
-}
